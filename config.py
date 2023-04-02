e5348f73692a47e2c437ea97ddce679121ef76ef
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-04-03 06:23:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", 0))
-STRING = getenv("STRING", "")
+API_ID = int(getenv("API_ID", 27547356))
+API_HASH = getenv("API_HASH", "9a5fa029f05c61096bf9fe89cc0f8862")
+BOT_TOKEN = getenv("BOT_TOKEN", "6240883713:AAFVtcJF4Ocef_uvovOJvg8vHkdLxpiLPr4")
+OWNER_ID = int(getenv("OWNER_ID", 1430103930))
+STRING = getenv("STRING", "AQDEBE3iIoEG0bvnzFIPPqcWWCF_FItDiyDgvk0bnoHv4IAM7zna-F56vgbq2bYUgdCnpS6UUMvcJDO75XrzXpOqNYDW0IrJlyZYprAfC8nrRCSSp5cCEQG6kvfF7TgdoYYmg9TRjZTU06mlEnra-WyDN_km3ZAUwVLefu7p_JTkKTz4mqjxsPzM4oDHYjuypIEXksHT5BmvrwZLILEugQxyvxvf9J2flpgdD_c6rkt4Qd9WcuAS9RTZhhqgH7C9cGdOQXxwCfrsu61cnzxG97t_KYovjP73c9kyM-sKGJfzKiB5W3PZvxUBCT9nrI692JQVtlys5r53CS_G_XxU_GpAVT2negA")
